e974cb92a0f5f60f9aa12e74018251f2ad1897a9
Garrett Dengler <garrett@dnglr.com>
2019-03-21 23:33:28 -0400

    spinning in spaaaaaaaace

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('body')
-
+<div class="grid__space" aria-hidden="true"></div>
 <div class="grid is-hidden" aria-hidden="true">
   <div class="grid__plane">
     @for ($i = 1; $i < 100; $i++)
