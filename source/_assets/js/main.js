0144525575b607460b461f71dafc19c0dc8913f3
Garrett Dengler <garrett@dnglr.com>
2019-03-21 22:53:44 -0400

    Init and grid stuff

---
+++ source/_assets/js/main.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+
+
+  setTimeout(function(){
+    document.querySelector('.grid.is-hidden').className = 'grid';
+  }, 500);
+
+};
---
+++ source/index.blade.php
@@ -0,0 +1,13 @@
+@extends('_layouts.master')
+
+@section('body')
+
+<div class="grid is-hidden" aria-hidden="true">
+  <div class="grid__plane">
+    @for ($i = 1; $i < 100; $i++)
+      <span class="grid__line grid__line--{{$i}}"></span>
+    @endfor
+  </div>
+</div>
+
+@endsection
