794d2d820a0f32a5d954d04b60e82985adfcef75
Garrett Dengler <garrett@dnglr.com>
2019-03-24 10:31:18 -0400

    Grid ios crash fix

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -17,14 +17,22 @@ var Site = {
       }, numLogos*300);
     }, 200);
 
+    var animateGrid = function(){
+      if ($(window).scrollTop() > 0) {
+        var rot = 30 - ($(window).scrollTop() / 26);
+        console.log('rotateX('+ ((rot > 0) ? rot : 0) +'deg) rotateY(0deg) rotateZ(0deg) translate3d(-50%, 0, 0)');
+        $('.grid__plane').css('transform', 'rotateX(' + ((rot > 0) ? rot : 0) + 'deg) rotateY(0deg) rotateZ(0deg) translate3d(-50%, 0, 0)');
+      }
+    };
+
     document.addEventListener('scroll', (evt) => {
 
       // var persp = 200 - ($(window).scrollTop() / 4);
       // $('.grid').css('perspective', (persp > 0) ? persp + 'px' : '0px');
 
-      var rot = 30 - ($(window).scrollTop() / 26);
-      console.log('rotateX('+ ((rot > 0) ? rot : 0) +'deg) translateX(-50%) translateZ(0px)');
-      $('.grid__plane').css('transform', 'rotateX(' + ((rot > 0) ? rot : 0) + 'deg) translateX(-50%) translateZ(0px)')
+      
+      window.requestAnimationFrame(animateGrid);
+
     }, {
       capture: true,
       passive: true
